e987666e752fa0617fee5c815fd09ffe2c3139c9
Nick Mather <nickmather12@gmail.com>
2020-10-01 17:28:51 +1000

    singleplayer and local multiplayer now working

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -2,6 +2,11 @@
 //it has the rules for the game, win conditions etc
 //it doesn't identify with the DOM
 
+const cellSelector = function() {
+  const min = Math.ceil(1);
+  const max = Math.floor(10);
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
 const gameBoard = {
   c1: '',
@@ -14,52 +19,111 @@ const gameBoard = {
   c8: '',
   c9: '',
 
-  player1: 'X',
-  player2: 'O',
+  player1: { name: 'X', score: 0},
+  player2: { name: 'O', score: 0},
 
-  lastTurn: 'O',
+  cpuLast: '',
+
   nextTurn: 'X',
   winner: '',
 
   play: function (cell) {
     if (this[cell] !== '') {
 
-    } else if  (this.nextTurn === this.player1) {
+    } else if  (this.nextTurn === this.player1.name) {
       this[cell] = 'X';
-      this.lastTurn = this.player1
-      this.nextTurn = this.player2;
+      this.nextTurn = this.player2.name;
     } else {
       this[cell] = 'O';
-      this.lastTurn = this.player2;
-      this.nextTurn = this.player1;
+      this.nextTurn = this.player1.name;
+    }
+  },
+
+  easyAiPlay: function (cell) {
+    if (this[cell] !== '') {
+    } else  {
+      this[cell] = 'X';
+      this.nextTurn = this.player2.name;
+
+      for (let i = 0; i < 9; i++) {
+        this.cpuLast = cellSelector();
+        aiCell = 'c' + this.cpuLast
+        console.log(aiCell);
+        //if it's empty, play in it
+        if (this[aiCell] === '') {
+          this[aiCell] = 'O';
+          this.nextTurn = this.player1.name;
+          break;
+        }
+      }
     }
   },
 
   winCheck: function() {
     //top row
     if (this.c1 != '' && this.c1 === this.c2 && this.c2 === this.c3) {
-      this.winner = this.lastTurn
+      if (this.c1 === 'X') {
+        this.winner = this.player1.name
+      } else if (this.c1 === 'O') {
+        this.winner = this.player2.name
+      }
+    console.log('top row')
     //first column
     } else if (this.c1 !== '' && this.c1 === this.c4 && this.c4 === this.c7) {
-      this.winner = this.lastTurn
+      if (this.c1 === 'X') {
+        this.winner = this.player1.name
+      } else if (this.c1 === 'O') {
+        this.winner = this.player2.name
+      }
+      console.log('first column')
     //middle Row
     } else if (this.c4 !== '' && this.c4 === this.c5 && this.c5 === this.c6) {
-      this.winner = this.lastTurn
+      if (this.c4 === 'X') {
+        this.winner = this.player1.name
+      } else if (this.c4 === 'O') {
+        this.winner = this.player2.name
+      }
+      console.log('middle row')
     //second column
     } else if (this.c2 !== '' && this.c2 === this.c5 && this.c5 === this.c8) {
-      this.winner = this.lastTurn
+      if (this.c2 === 'X') {
+        this.winner = this.player1.name
+      } else if (this.c2 === 'O') {
+        this.winner = this.player2.name
+      }
+      console.log('second row')
     //bottom row
     } else if (this.c7 !== '' && this.c7 === this.c8 && this.c8 === this.c9) {
-      this.winner = this.lastTurn
+      if (this.c7 === 'X') {
+        this.winner = this.player1.name
+      } else if (this.c7 === 'O') {
+        this.winner = this.player2.name
+      }
+      console.log('bottom row')
     //third Column
     } else if (this.c3 !== '' && this.c3 === this.c6 && this.c6 === this.c9) {
-      this.winner = this.lastTurn
+      if (this.c3 === 'X') {
+        this.winner = this.player1.name
+      } else if (this.c3 === 'O') {
+        this.winner = this.player2.name
+      }
+      console.log('third column')
     //left diagonal
     } else if (this.c1 !== '' && this.c1 === this.c5 && this.c5 === this.c9) {
-      this.winner = this.lastTurn
+      if (this.c1 === 'X') {
+        this.winner = this.player1.name
+      } else if (this.c1 === 'O') {
+        this.winner = this.player2.name
+      }
+      console.log('left diagonal')
     //right diagonal
     } else if (this.c3 !== '' && this.c3 === this.c5 && this.c5 === this.c7) {
-      this.winner = this.lastTurn
+      if (this.c3 === 'X') {
+        this.winner = this.player1.name
+      } else if (this.c3 === 'O') {
+        this.winner = this.player2.name
+      }
+      console.log('right diagonal')
     //draw condition
     } else if (
       this.c1 !== '' &&
@@ -73,6 +137,16 @@ const gameBoard = {
       this.c9 !== ''
     ) {
       this.winner = 'draw'
+    }
+  },
+
+  scoreUpdate: function () {
+    if (this.winner !== '') {
+      if (this.winner === this.player1.name) {
+        this.player1.score ++
+      } else if (this.winner === this.player2.name) {
+        this.player2.score ++
+    }
     }
   }
 };
--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -23,45 +23,86 @@ const tokens = [
 
 let $token1 = tokens[0];
 let $token2 = tokens[1];
+let $tokenSp = tokens[0];
+
+let mode;
 
 const local = function() {
   $('#modeSelect').addClass('hidden');
   $('#localMode').removeClass('hidden');
 }
 
-const back = function() {
+const single = function() {
+  $('#modeSelect').addClass('hidden');
+  $('#singleplayer').removeClass('hidden');
+}
+
+const home = function() {
   $('#localMode').addClass('hidden');
+  $('#singleplayer').addClass('hidden');
+  $('.game').addClass('hidden')
   $('#modeSelect').removeClass('hidden');
+  $('.menus').removeClass('hidden');
+  stopGame();
 }
 
 
-const updateSettings = function () {
+const startLocal = function () {
   const p1Name = $('#p1Name').val();
   const p2Name = $('#p2Name').val();
-  gameBoard.player1 = p1Name;
-  gameBoard.player2 = p2Name;
+  gameBoard.player1.name = p1Name;
+  gameBoard.player2.name = p2Name;
   gameBoard.nextTurn = p1Name;
-  console.log(gameBoard.player1);
-  console.log(gameBoard.player2);
   $('.menus').addClass('hidden');
   $('.game').removeClass('hidden');
+  gameBoard.player1.score = 0
+  gameBoard.player2.score = 0
+  mode = 'local'
+  render();
+}
+
+function randomToken() {
+  return Math.floor(Math.random() * Math.floor(tokens.length));
+}
+
+const startSingle = function () {
+  const p1Name = $('#spName').val();
+  const p2Name = 'Computer'
+  $token1 = $tokenSp
+  $token2 = tokens[randomToken()]
+  gameBoard.player1.name = p1Name;
+  gameBoard.player2.name = p2Name;
+  gameBoard.nextTurn = p1Name;
+  $('.menus').addClass('hidden');
+  $('.game').removeClass('hidden');
+  gameBoard.winner = '';
+  gameBoard.player1.score = 0;
+  gameBoard.player2.score = 0;
+  mode = 'single'
   render();
 }
 
 const render = function () {
   if (gameBoard.winner !== '') {
-    //disable clicks then display win + reset button
-    stopGame()
+    //disable clicks, then display win + reset button
+    gameBoard.scoreUpdate()
+    stopGame();
     if (gameBoard.winner !== 'draw') {
       $('#messages').text(`Congratulations ${gameBoard.winner}, You win!`)
     } else {
       $('#messages').text(`Cat's Game! It's a draw.`)
     }
-    $('#reset').removeClass('hidden');
+    $('#reset, #menu-back').removeClass('hidden');
   } else {
     $('#messages').text(`It's ${gameBoard.nextTurn}'s turn`)
-    // render box content
   }
+  $('#p1-score p').text(
+    `${gameBoard.player1.name}: ${gameBoard.player1.score} wins`
+  )
+  $('#p2-score p').text (
+    `${gameBoard.player2.name}: ${gameBoard.player2.score} wins`
+  )
+  // render box content
   printToken('c1', "#1")
   printToken('c2', "#2")
   printToken('c3', "#3")
@@ -103,63 +144,150 @@ const reset = function () {
 }
 
 const startGame = function () {
-  $('#1').on('click', function () {
-    gameBoard.play('c1');
-    gameBoard.winCheck();
-    $('#1').removeClass('marker').addClass(`${gameBoard.c1}`);
+  if (mode === 'local') {
+    $('#1').on('click', function () {
+      gameBoard.play('c1');
+      gameBoard.winCheck();
+      $('#1').removeClass('marker').addClass(`${gameBoard.c1}`);
+      render();
+    })
+    $('#2').on('click', function () {
+      gameBoard.play('c2');
+      gameBoard.winCheck();
+      $('#2').removeClass('marker').addClass(`${gameBoard.c2}`);
+      render();
+    })
+    $('#3').on('click', function () {
+      gameBoard.play('c3');
+      gameBoard.winCheck();
+      $('#3').removeClass('marker').addClass(`${gameBoard.c3}`);
+      render();
+    })
+    $('#4').on('click', function () {
+      gameBoard.play('c4');
+      gameBoard.winCheck();
+      $('#4').removeClass('marker').addClass(`${gameBoard.c4}`);
+      render();
+    })
+    $('#5').on('click', function () {
+      gameBoard.play('c5');
+      gameBoard.winCheck();
+      $('#5').removeClass('marker').addClass(`${gameBoard.c5}`);
+      render();
+    })
+    $('#6').on('click', function () {
+      gameBoard.play('c6');
+      gameBoard.winCheck();
+      $('#6').removeClass('marker').addClass(`${gameBoard.c6}`);
+      render();
+    })
+    $('#7').on('click', function () {
+      gameBoard.play('c7');
+      gameBoard.winCheck();
+      $('#7').removeClass('marker').addClass(`${gameBoard.c7}`);
+      render();
+    })
 
-    render();
-  })
-  $('#2').on('click', function () {
-    gameBoard.play('c2');
-    gameBoard.winCheck();
-    $('#2').removeClass('marker').addClass(`${gameBoard.c2}`);
-    render();
-  })
-  $('#3').on('click', function () {
-    gameBoard.play('c3');
-    gameBoard.winCheck();
-    $('#3').removeClass('marker').addClass(`${gameBoard.c3}`);
-    render();
-  })
-  $('#4').on('click', function () {
-    gameBoard.play('c4');
-    gameBoard.winCheck();
-    $('#4').removeClass('marker').addClass(`${gameBoard.c4}`);
-    render();
-  })
-  $('#5').on('click', function () {
-    gameBoard.play('c5');
-    gameBoard.winCheck();
-    $('#5').removeClass('marker').addClass(`${gameBoard.c5}`);
-    render();
-  })
-  $('#6').on('click', function () {
-    gameBoard.play('c6');
-    gameBoard.winCheck();
-    $('#6').removeClass('marker').addClass(`${gameBoard.c6}`);
-    render();
-  })
-  $('#7').on('click', function () {
-    gameBoard.play('c7');
-    gameBoard.winCheck();
-    $('#7').removeClass('marker').addClass(`${gameBoard.c7}`);
-    render();
-  })
+    $('#8').on('click', function () {
+      gameBoard.play('c8');
+      gameBoard.winCheck();
+      $('#8').removeClass('marker').addClass(`${gameBoard.c8}`);
+      render();
+    })
 
-  $('#8').on('click', function () {
-    gameBoard.play('c8');
-    gameBoard.winCheck();
-    $('#8').removeClass('marker').addClass(`${gameBoard.c8}`);
-    render();
-  })
+    $('#9').on('click', function () {
+      gameBoard.play('c9');
+      gameBoard.winCheck();
+      $('#9').removeClass('marker').addClass(`${gameBoard.c9}`);
+      render();
+    })
 
-  $('#9').on('click', function () {
-    gameBoard.play('c9');
-    gameBoard.winCheck();
-    $('#9').removeClass('marker').addClass(`${gameBoard.c9}`);
-    render();
-  })
+  } else if (mode === 'single') {
+    $('#1').on('click', function () {
+      console.log('click')
+      gameBoard.easyAiPlay('c1');
+      gameBoard.winCheck();
+      $('#1').removeClass('marker').addClass(`${gameBoard.c1}`);
+      $('#' + gameBoard.cpuLast)
+        .removeClass('marker')
+        .addClass(`${gameBoard['c' + gameBoard.cpuLast]}`);
+      render();
+    })
+    $('#2').on('click', function () {
+      gameBoard.easyAiPlay('c2');
+      gameBoard.winCheck();
+      $('#2').removeClass('marker').addClass(`${gameBoard.c2}`);
+      $('#' + gameBoard.cpuLast)
+        .removeClass('marker')
+        .addClass(`${gameBoard['c' + gameBoard.cpuLast]}`);
+      render();
+    })
+    $('#3').on('click', function () {
+      gameBoard.easyAiPlay('c3');
+      gameBoard.winCheck();
+      $('#3').removeClass('marker').addClass(`${gameBoard.c3}`);
+      $('#' + gameBoard.cpuLast)
+        .removeClass('marker')
+        .addClass(`${gameBoard['c' + gameBoard.cpuLast]}`);
+      render();
+    })
+    $('#4').on('click', function () {
+      gameBoard.easyAiPlay('c4');
+      gameBoard.winCheck();
+      $('#4').removeClass('marker').addClass(`${gameBoard.c4}`);
+      $('#' + gameBoard.cpuLast)
+        .removeClass('marker')
+        .addClass(`${gameBoard['c' + gameBoard.cpuLast]}`);
+      render();
+    })
+    $('#5').on('click', function () {
+      gameBoard.easyAiPlay('c5');
+      gameBoard.winCheck();
+      $('#5').removeClass('marker').addClass(`${gameBoard.c5}`);
+      $('#' + gameBoard.cpuLast)
+        .removeClass('marker')
+        .addClass(`${gameBoard['c' + gameBoard.cpuLast]}`);
+      render();
+    })
+    $('#6').on('click', function () {
+      gameBoard.easyAiPlay('c6');
+      gameBoard.winCheck();
+      $('#6').removeClass('marker').addClass(`${gameBoard.c6}`);
+      $('#' + gameBoard.cpuLast)
+        .removeClass('marker')
+        .addClass(`${gameBoard['c' + gameBoard.cpuLast]}`);
+      render();
+    })
+    $('#7').on('click', function () {
+      gameBoard.easyAiPlay('c7');
+      gameBoard.winCheck();
+      $('#7').removeClass('marker').addClass(`${gameBoard.c7}`);
+      $('#' + gameBoard.cpuLast)
+        .removeClass('marker')
+        .addClass(`${gameBoard['c' + gameBoard.cpuLast]}`);
+      render();
+    })
+
+    $('#8').on('click', function () {
+      gameBoard.easyAiPlay('c8');
+      gameBoard.winCheck();
+      $('#8').removeClass('marker').addClass(`${gameBoard.c8}`);
+      $('#' + gameBoard.cpuLast)
+        .removeClass('marker')
+        .addClass(`${gameBoard['c' + gameBoard.cpuLast]}`);
+      render();
+    })
+
+    $('#9').on('click', function () {
+      gameBoard.easyAiPlay('c9');
+      gameBoard.winCheck();
+      $('#9').removeClass('marker').addClass(`${gameBoard.c9}`);
+      $('#' + gameBoard.cpuLast)
+        .removeClass('marker')
+        .addClass(`${gameBoard['c' + gameBoard.cpuLast]}`);
+      render();
+    })
+  }
 }
 
 const stopGame = function () {
@@ -174,63 +302,128 @@ const stopGame = function () {
   $('#9').off('click');
 }
 
-let token1I = 1;
-let token2I = 2;
+let token1I = 0;
+let token2I = 1;
+let tokenSpI = 0;
+
 const pickToken1 = function (dir) {
-  console.log(token1I)
   if (dir === 'right') {
-    if (token1I < tokens.length) {
-      $token1 = tokens[token1I];
+    if (token1I < tokens.length -1 ) {
       token1I ++
+      $token1 = tokens[token1I];
     } else {
     token1I = 0
     $token1 = tokens[token1I];
-    token1I ++
     }
-  } else {
+
+  } else if (dir === 'left') {
     if (token1I > 0) {
-      $token1 = tokens[token1I];
       token1I --
+      $token1 = tokens[token1I];
     } else {
-      token1I = tokens.length;
+      token1I = tokens.length -1;
       $token1 = tokens[token1I];
-      token1I --
     }
-    //this needs bugfixing!!
   };
-  console.log(token1I)
   $(".menus #p1 img").remove();
   $(".menus #p1").append($token1)
 };
 
+const pickToken2 = function (dir) {
+  if (dir === 'right') {
+    if (token2I < tokens.length -1 ) {
+      token2I ++
+      $token2 = tokens[token2I];
+    } else {
+    token2I = 0
+    $token2 = tokens[token2I];
+    }
+
+  } else if (dir === 'left') {
+    if (token2I > 0) {
+      token2I --
+      $token2 = tokens[token2I];
+    } else {
+      token2I = tokens.length -1;
+      $token2 = tokens[token2I];
+    }
+  };
+  $(".menus #p2 img").remove();
+  $(".menus #p2").append($token2);
+};
+
+const pickTokenSp = function (dir) {
+  if (dir === 'right') {
+    console.log(tokenSpI)
+    if (tokenSpI < tokens.length -1 ) {
+      tokenSpI ++
+      $tokenSp = tokens[tokenSpI];
+    } else {
+    tokenSpI = 0
+    $tokenSp = tokens[tokenSpI];
+    }
+
+  } else if (dir === 'left') {
+    if (tokenSpI > 0) {
+      tokenSpI --
+      $tokenSp = tokens[tokenSpI];
+    } else {
+      tokenSpI = tokens.length -1;
+      $tokenSp = tokens[tokenSpI];
+    }
+  };
+  $(".menus #sp img").remove();
+  $(".menus #sp").append($tokenSp);
+  console.log(tokenSpI)
+};
+
 $(document).ready(function () {
-
-  startGame();
-  render();
-
+//local multiplayer buttons
   $('#local').on('click', function() {
     local();
   })
-
   $('#back').on('click', function() {
-    back();
+    home();
   })
-
   $('#go').on('click', function() {
-    updateSettings();
+    startLocal();
     reset();
   })
-
-  $('#reset').on('click', function() {
-    reset();
-  })
-
   $('#p1-img-right').on('click', function() {
     pickToken1('right')
   })
   $('#p1-img-left').on('click', function() {
-    console.log('click')
     pickToken1('left')
   })
+  $('#p2-img-right').on('click', function() {
+    pickToken2('right')
+  })
+  $('#p2-img-left').on('click', function() {
+    pickToken2('left')
+  })
 
+  //singleplayer buttons
+  $('#single').on('click', function () {
+    single();
+  })
+  $('#sp-img-right').on('click', function() {
+    pickTokenSp('right')
+  })
+  $('#sp-img-left').on('click', function() {
+    pickTokenSp('left')
+  })
+  $('#back-sp').on('click', function() {
+    home();
+  })
+  $('#go-sp').on('click', function() {
+    startSingle();
+    reset();
+  })
+  //game board buttons
+  $('#reset').on('click', function() {
+    reset();
+  })
+  $('#menu-back').on('click', function() {
+    home();
+  })
 });
